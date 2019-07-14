ae7658df70adc0cf5d160e981352e9295fc405ba
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-15 04:08:42 +0700

    commit

--- views/setting/admin_view.php
+++ views/setting/admin_view.php
@@ -62,7 +62,7 @@ $attributes = [
 	],
 	[
 		'attribute' => '',
-		'value' => Html::a(Yii::t('app', 'Update'), ['update', 'id'=>$model->primaryKey], ['title'=>Yii::t('app', 'Update'), 'class'=>'btn btn-primary']),
+		'value' => Html::a(Yii::t('app', 'Update'), ['update'], ['title'=>Yii::t('app', 'Update'), 'class'=>'btn btn-primary']),
 		'format' => 'html',
 	],
 ];
