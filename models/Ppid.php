d977498144d4eeb995cb6dc35e5dc2fec01ae608
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-10-22 17:54:56 +0700

    add condition jika model tidak dipanggil pada mode search (gridview or with dataProvider)

--- models/Ppid.php
+++ models/Ppid.php
@@ -165,6 +165,9 @@ class Ppid extends \app\components\ActiveRecord
 		if(!(Yii::$app instanceof \app\components\Application))
 			return;
 
+		if(!$this->hasMethod('search'))
+			return;
+
 		$this->templateColumns['_no'] = [
 			'header' => '#',
 			'class' => 'yii\grid\SerialColumn',
